894721371d62f23933b4f9467bcbb4423adda753
김재준 <kimsman@gimjaejun-ui-MacBookPro.local>
2023-01-02 00:48:01 +0900

    contact 페이지 수정

--- pages/contact.js
+++ pages/contact.js
@@ -1,23 +1,45 @@
 import Layout from "../components/layout";
+import Head from "next/head";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+import { useTheme } from "next-themes";
+import { useState } from "react";
 
 export default function Contact() {
+  const { theme, setTheme } = useTheme();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [msg, setMsg] = useState("");
+
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    const data = {
-      name: e.target.name.value,
-      email: e.target.email.value,
-      msg: e.target.message.value,
-    };
-
     const res = await fetch("/api/contact", {
       method: "POST",
-      body: JSON.stringify(data),
+      body: JSON.stringify({ name, email, msg }),
     });
+
+    setName("");
+    setEmail("");
+    setMsg("");
+
+    if (res.status === 201) {
+      toast("연락해 주셔서 감사합니다.", { type: "success" });
+    } else {
+      toast("입력이 제대로 되지 않았습니다. 다시 확인해주세요.", {
+        type: "error",
+      });
+    }
   };
 
   return (
     <Layout>
+      <Head>
+        <title>김재준의 포트폴리오 | 연락하기</title>
+        <meta name="description" content="연락하기" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ToastContainer theme={theme === "dark" ? "dark" : "light"} />
       <form className="text-gray-600 body-font relative" onSubmit={onSubmit}>
         <div className="container px-5 py-24 mx-auto min-h-screen ">
           <div className="flex flex-col text-center w-full mb-12">
@@ -43,6 +65,8 @@ export default function Contact() {
                     type="text"
                     id="name"
                     name="name"
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
                     className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                   />
                 </div>
@@ -60,6 +84,8 @@ export default function Contact() {
                     type="email"
                     id="email"
                     name="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
                     className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                   />
                 </div>
@@ -76,6 +102,8 @@ export default function Contact() {
                     required
                     id="message"
                     name="message"
+                    onChange={(e) => setMsg(e.target.value)}
+                    value={msg}
                     className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
                   ></textarea>
                 </div>
