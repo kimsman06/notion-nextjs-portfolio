ecaeb1d55f380deef4bc1125602a51e6bec866d8
김재준 <kimsman@gimjaejun-ui-MacBookPro.local>
2023-02-21 23:43:21 +0900

    correct image error

--- pages/projects.js
+++ pages/projects.js
@@ -2,6 +2,7 @@ import Layout from "../components/layout";
 import Head from "next/head";
 import ProjectItem from "../components/projects/project-item";
 import { TOKEN, DATABASE_ID } from "../config/index";
+import { useState } from "react";
 
 export default function Projects({ projects }) {
   return (
@@ -11,7 +12,6 @@ export default function Projects({ projects }) {
         <meta name="description" content="제작한 영상들을 볼 수 있습니다." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <div className="container mx-auto flex px-5 py-5 md:flex-row flex-col items-center">
         <div className="flex flex-col items-center justify-center min-h-screen mb-10">
           <h1 className="text-2xl font-bold md:text-4xl mb-8">
@@ -20,7 +20,6 @@ export default function Projects({ projects }) {
               {projects.results.length}
             </span>
           </h1>
-
           <div className="grid gird-cols-1 gap-8 md:grid-cols-2 2xl:grid-cols-4">
             {projects.results.map((aProject) => (
               <ProjectItem key={aProject.id} data={aProject} />
@@ -55,7 +54,6 @@ export async function getStaticProps() {
       ],
     }),
   };
-
   const res = await fetch(
     `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
     options
