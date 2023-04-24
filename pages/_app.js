16151e403f314fbf96ef32f3230db34e3f7e1080
김재준 <kimsman@gimjaejun-ui-MacBookPro.local>
2023-04-24 23:56:43 +0900

    update loading bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,24 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import NProgress from "nprogress";
+import Router from "next/router";
+import Head from "next/head";
+
+NProgress.configure({ showSpinner: false });
+Router.events.on("routeChangeStart", (url) => {
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", (url) => {
+  NProgress.done(false);
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
